ea993011857f8d6a1be51d8aa70a6604707b5a25
saurabhkumarverma <saurabh88871kumar@hotmail.com>
2020-05-21 15:49:35 +0530

    Database change
    1> Sqlite to Postgres
    2>Gitignores file is added

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'producthunt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'producthuntdb',
+        'USER': 'postgres',
+        'PASSWORD':'A1995@kit',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
