36d31ba4d9ae10761404c72fd30dd51bdd18c0f9
saurabhkumarverma <saurabh88871kumar@hotmail.com>
2020-06-30 10:45:41 +0530

    Logout
    Logout is working after login sucessfully

--- accounts/views.py
+++ accounts/views.py
@@ -20,7 +20,29 @@ def signup(request):
         return render(request,'accounts/signup.html')
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username = request.POST['username'],password = request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+
+
+        else:
+            return render(request, 'accounts/login.html',{'error':'username or password is incorrect.'})
+
+    else:
+        return render(request, 'accounts/Login.html')
+
+
+
+
+
+
+
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+
     return render(request,'accounts/signup.html')
